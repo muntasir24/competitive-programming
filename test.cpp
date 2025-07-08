202a6dffc50ec72a459903285f9ab3a07a67c4b1
Aziz Muntasir <160031061+muntasir24@users.noreply.github.com>
2025-07-08 23:21:58 +0600

    cp

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+  
+
+int main(){
+
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    ll test_case;
+   
+    cin >> test_case;
+    
+    while (test_case--)
+    {  
+
+   cout<<test_case+1;
+
+
+
+//cout << (flag ==1 ? "YES" : "NO") << "\n";
+
+ }
+    
+    return 0;
+}
