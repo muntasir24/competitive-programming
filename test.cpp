f30644921f754948ba7efd828fcc054a2d2f3dcc
Aziz Muntasir <160031061+muntasir24@users.noreply.github.com>
2025-07-13 11:04:26 +0600

    auto commit

--- test.cpp
+++ test.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -11,106 +10,76 @@ ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
     ll test_case;
+   
     cin >> test_case;
+    
     while (test_case--)
     {  
- 
 
-ll n,m;
-cin>>n>>m;
-vector<vector<int>>v(n,vector<int>(m,0));
-vector<vector<int>>ans(n,vector<int>(m,0));
-vector<ll> vt;
-for (int i = 0; i < n; ++i)
-{
-    for (int j = 0; j < m; ++j)
-    {
-        cin>>v[i][j];
-        vt.push_back(v[i][j]);
-    }
-}
-sort(vt.begin(),vt.end());
+   
+int n,p1,p2,tp,ttp,tp2,tp1;
+cin>>n;
+vector<int>v(n);
+map<int,int>mp;
 
 for (int i = 0; i < n; ++i)
 {
-    sort(v[i].rbegin(),v[i].rend());
+   cin>>v[i];
+   mp[v[i]]=i+1;
 }
 
-// for (int i = 0; i < n; ++i)
-// {
-//     for (int j = 0; j < m; ++j)
-//     {
-//         cout<<v[i][j]<<" ";
-//     }
-//     cout<<endl;
-// }
-int found,ok,idx1,idx2,r=0,c=0;
-for (int i = 0; i < m; ++i)
+p1=1,p2=n;
+int flag=0;
+for (int i = 0; i < n; ++i)
+{
+   tp=v[i];
+   if(tp==p1){
+      p1++;
+   }
+   else if(tp==p2){
+      p2--;
+   }
+   else{
+      ttp=v[n-1];
+      tp1=p1;
+    tp2=p2;
 
-{ 
-	
-	found=0;
-    for (int j = 0; j < n; ++j)
-    {
-       
-        idx1=-1;
-        for (int k = 0; k < m; ++k)
-        {
-            if(vt[i]==v[j][k] and found==0){
-             found=1;
-             idx1=j;
-             idx2=k;
-             break;
-            }
-
-            if((v[j][k]!=0 and idx1==-1)){
-            	idx1=j;
-            	idx2=k;
-            }
-
-            
-        }
-        if(found==1){
-found++;
-ans[j][i]=v[idx1][idx2];
- v[idx1][idx2]=0;
-        }
-        else{
-        	ans[j][i]=v[idx1][idx2];
-        	v[idx1][idx2]=0;
-        }
-        
-
-
+      int k=n-1;
+      while(i<k){
+if(ttp==tp1){
+tp1++;
+}
+if(ttp==tp2){
+tp2--;
+}
+else{
+   //ans
+vector<int> vc = {tp, ttp, tp1, tp2};
+    sort(vc.begin(), vc.end());
+    if(vc[0]!=ttp and vc[3]!=ttp and vc[0]!=tp and vc[3]!=tp ){
+      flag=1;
+      break;
     }
-// cout<<"for : "<<i<<endl;
-//     for (int l = 0; l <n ; ++l)
-//     {
-//     	for (int r = 0; r < m; ++r)
-//     	{
-//     		cout<<v[l][r]<<" ";
-//     	}
-//     	cout<<endl;
-//     }
-//     cout<<endl;
+
+
+
+}
+k--;
+      }
+   }
+   if(flag)break;
    
 }
 
-
-    for (int l = 0; l <n ; ++l)
-    {
-    	for (int r = 0; r < m; ++r)
-    	{
-    		cout<<ans[l][r]<<" ";
-    	}
-    	cout<<endl;
-    }
-   
+if(flag){
+   cout<<min(mp[ttp],mp[tp])<<" "<<max(mp[ttp],mp[tp])<<endl;
+}
+else 
+cout<<"-1"<<endl;
 
 //cout << (flag ==1 ? "YES" : "NO") << "\n";
 
-    }
+ }
     
     return 0;
 }
-
